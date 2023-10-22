bb72276cfde2a9a10b6c5448bdb937e1c5bfb9fd
S. Manannikov <cerzey5@yandex.ru>
2023-10-23 06:13:10 +0700

    пример

---
+++ project/GameEngine/ExampleFile.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Windows.Forms;
+
+namespace GameProject.GameEngine//пример класса
+{
+    public class GameEngine
+    {
+        // Поле для хранения текущего состояния игры
+        private int gameState;
+
+        // Конструктор класса
+        public GameEngine()
+        {
+            // Инициализация игрового состояния
+            gameState = 1;
+        }
+
+        // Метод для обновления состояния игры
+        public void Update()
+        {
+            // Здесь происходит обновление игры, расчет физики, логики и т.д.
+            gameState = 2;
+        }
+
+        // Метод для отрисовки игры
+        public void Render()
+        {
+            // Здесь происходит отрисовка игры на экране
+            MessageBox.Show(gameState.ToString());
+        }
+    }
+}
