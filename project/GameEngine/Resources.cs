df3599fe83df5df3d9bec917853bdee237ef0772
PervovAndrey <andrey.pervov05@mail.ru>
2023-11-06 21:39:51 +0700

    06.11.2023

---
+++ project/GameEngine/Resources.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GameProject.GameEngine
+{
+    /* Просто хранилище для текстур и строк текста, 
+     * и другого рода таких данных, 
+     * так же реализует работу с файловой системой
+     */
+
+    internal class Resources
+    {
+
+        private Textures _textures;
+        private Options _options;
+
+        public void LoadLevel(string name)
+        {
+        
+
+
+        }
+
+        public void LoadTexture(string name)
+        {
+        
+        
+        }
+        
+        public void GetTexture(string name)
+        {
+        
+        
+        
+        }
+    }
+}
