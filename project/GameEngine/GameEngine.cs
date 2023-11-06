0c6833d939c9ff955e0c95d98d888f853d150082
S. Manannikov <cerzey5@yandex.ru>
2023-11-06 19:20:02 +0700

    Im_added_gameengine_06_11_23

---
+++ project/GameEngine/GameEngine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject.GameEngine
+{
+
+    private Physics _physics;
+    private Resourses _resoucses;
+    private GameLogic _gameLogic;
+
+    /*
+     Основной модуль игры, который будет вызываться в отдельном потоке
+     */
+    public class GameEngine
+    {
+
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop() 
+        {
+        
+        
+        }
+    }
+}
